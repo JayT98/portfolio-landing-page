c444d4e0567cdd66617b1af72fa703973aa1ac4f
JayT98 <kientrung2509@gmail.com>
2023-02-23 17:54:08 +1100

    Upload all file and waitting merging

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $('.scroll-top').hide();
+    $('#menu-btn').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('active');
+    });
+
+
+    $(window).on('scroll',function(){
+        $('#menu-btn').removeClass('fa-times');
+        $('.navbar').removeClass('active');
+
+        if($(window).scrollTop() > 0){
+            $(".header").addClass("sticky");
+        }else{
+            $(".header").removeClass("sticky");
+        }
+
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('.scroll-top').fadeIn();
+            } else {
+                $('.scroll-top').fadeOut();
+            }
+        });
+    });
+});
